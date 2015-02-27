25699962a577aa84e92d2305d8dbe3366c549e97
Per Hellström <perhells@kth.se>
2015-02-27 12:55:31 +0100

    Added comment and large straight check.

--- main.py
+++ main.py
@@ -1,11 +1,30 @@
 import random
 
+die = [1,2,3,4,5]
 
 def roll():
 	return random.randrange(1,7)
-for j in range(0,39000000):
-	die = []
-	for i in range(0,5):
-		die.append(roll())
-	print(die)
 
+def hasStraight():
+	# There are 5! ways of rolling each of the large straights.
+	# This gives it a probability of 2*5!/6^5 = 240/7776 ≃ 0.031 on the first roll.
+	return 	1 in die and \
+			2 in die and \
+			3 in die and \
+			4 in die and \
+			5 in die\
+			or \
+			2 in die and \
+			3 in die and \
+			4 in die and \
+			5 in die and \
+			6 in die
+
+count = 0
+for j in range(0,390000):
+	for i in range(0,5):
+		die[i] = roll()
+
+	if hasStraight():
+		count += 1
+		print("Straight! (" + str(die) + " " + str(count) + " out of " + str(j) + " " + str(float(count)/float(j)) + ")")
